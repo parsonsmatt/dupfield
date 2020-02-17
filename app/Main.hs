8c912ba16b6c0268ccf9112aff9f02545e078b66
parsonsmatt <parsonsmatt@gmail.com>
2020-02-17 12:59:39 -0700

    more

--- app/Main.hs
+++ app/Main.hs
@@ -11,10 +11,14 @@ main :: IO ()
 main = do
     let x = Lib.X { X.name = "hello" }
     print x
+    print $ Lib.Y { Y.age = 3 }
+
     -- Using the field as an accessor fixes the warning.
     -- print (X.name x)
-    print $ Lib.Y { Y.age = 3 }
-    -- Using the field as an accessor fixes the warning.
     -- print (Y.age (Lib.Y 3))
 
 
+    -- Using the field in update syntax fixes the warning.
+    -- let y = Lib.Y 3
+    -- print y { Y.age = 4 }
+    -- print x { X.name = "no" }
